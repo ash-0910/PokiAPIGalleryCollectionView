ef15575743001b534de4814791517138a6717baa
ashish <ashishsurti0910@gmail.com>
2022-03-30 14:51:33 -0400

    created project and added functionalities

---
+++ PokiAPIGallery/PokeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PokeCollectionViewCell.swift
+//  PokiAPIGallery
+//
+//  Created by Ash on 2022-03-30.
+//
+
+import UIKit
+
+class PokeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokeimage: UIImageView!
+    
+}
